5e8a53a97faac854c03db33809aa05a0d82975de
Yoga Arta G <grahanantyo@gmail.com>
2022-06-22 21:10:17 +0700

    add login lagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route,  Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
-
+import Login from './page/Login';
 import Footer from "./component/Footer"
 import Navbar from "./component/Navbar"
 
@@ -9,8 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/footer' element={<Footer />} />    
-        <Route path='/navbar' element={<Navbar />} />     
+        <Route path='/login' element={<Login />} />
+        <Route path='/footer' element={<Footer />} />
+        <Route path='/navbar' element={<Navbar />} />
       </Routes>
     </Router>
   );
