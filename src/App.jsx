02a0e3e5e4658f6a14b2e07137e05d5fdb476b48
Zulfahmi321 <zulfahmi1729@gmail.com>
2022-06-23 00:29:20 +0700

    add home + responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
+// import './App.css';
 import Login from './page/Login';
 import Footer from "./component/Footer"
-import Navbar from "./component/Navbar"
-import Home from './page/home';
+// import Navbar from "./component/Navbar"
+import Home from './page/Home';
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/footer' element={<Footer />} />
-        <Route path='/navbar' element={<Navbar />} />
+        {/* <Route path='/navbar' element={<Navbar />} /> */}
       </Routes>
     </Router>
   );
