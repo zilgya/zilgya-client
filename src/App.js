0e0f29b292b6ac5efc6e24389a2301702cd74f9a
Yoga Arta G <grahanantyo@gmail.com>
2022-06-22 18:15:21 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Login from './page/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/page/Login/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import './Login.css'
+export default class Login extends Component {
+  render() {
+    return (
+      <>
+        <main className='login-global-container'>
+          <div className="login-header">
+            <div className="login-header-title">My Account</div>
+            <div className="login-header-info">Register and log in with your account to be able to shop at will</div>
+          </div>
+          <div className="login-main-container">
+            <div className="login-login-section">
+              <div className="login-register-title">Login</div>
+              <input type="text" className="login-input" placeholder='User name or email address' />
+              <input type="text" className="login-input" placeholder='Password' />
+              <div className="login-button">Login</div>
+              <div className="login-checkbox">
+                <label htmlFor="remember-me" className="login-customer">
+                  <input type="checkbox" name="remember-me" id="remember-me" />
+                  Remember me
+                </label>
+                <div className='login-forgot'>Forget your password?</div>
+              </div>
+            </div>
+            <div className="login-register-section">
+              <div className="login-register-title">Create Account</div>
+              <input type="text" className="login-input" placeholder='Email address' />
+              <input type="text" className="login-input" placeholder='Password' />
+              <div className="login-checkbox-register">
+                <label htmlFor="customer" className="login-customer">
+                  <input type="radio" name="role" id="customer" />
+                  I'm Customer
+                </label>
+                <label htmlFor="seller" className="login-customer">
+                  <input type="radio" name="role" id="seller" />
+                  I'm Seller
+                </label>
+              </div>
+              <div className="login-button">Register</div>
+            </div>
+          </div>
+        </main>
+      </>
+    )
+  }
+}
