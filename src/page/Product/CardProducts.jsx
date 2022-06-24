f7e171e152ff2da189d7adaf48c887c95fd0a862
kondelow <achmadrasidi@gmail.com>
2022-06-25 03:12:28 +0700

    implementation product all,sort,filter,pagination

--- src/page/Product/CardProducts.jsx
+++ src/page/Product/CardProducts.jsx
@@ -1,45 +1,80 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import React, { Component } from "react";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "./Pagination";
 
-class CardProducts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: this.props.id,
-      name: this.props.name,
-      price: this.props.price,
-      image: null,
-    };
+const CardProducts = ({ setTotalProduct, brand, setTotalCat, minPrice, maxPrice, filterPrice, category, sort, order }) => {
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState(null);
+  const [mta, setMta] = useState({});
+  const [pageUrl, setPageUrl] = useState(null);
+  let baseUrl = `${process.env.REACT_APP_HOST_API}/product?`;
+
+  if (sort) {
+    baseUrl += `sort=${sort}&order=${order}&`;
   }
-
-  async componentDidMount() {
-    try {
-      const result = await axios.get(`${process.env.REACT_APP_HOST_API}/product/images/${this.state.id}`);
-      this.setState({
-        image: result.data.data[0].url,
-      });
-    } catch (error) {
-      this.setState({
-        error: error.response ? error.response.data.err : error.message,
-      });
+  if (filterPrice) {
+    if (category) {
+      baseUrl += `categories=${category}&`;
+    }
+    if (brand) {
+      baseUrl += `brand=${brand}&`;
+    }
+    if (minPrice || maxPrice) {
+      baseUrl += `minPrice=${minPrice}&maxPrice=${maxPrice}&`;
     }
   }
-  render() {
-    const formatter = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 });
-    return (
-      <React.Fragment>
-        <div className="card col card-product">
-          <div className="card-product-img">
-            <img src={this.state.image} alt="" />
-          </div>
-          <div className="card-body">
-            <div className="card-product-name">{this.state.name}</div>
-            <div className="card-product-price">IDR {formatter.format(this.state.price).split("Rp")[1]}</div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
+  if (!!pageUrl) {
+    baseUrl += pageUrl;
   }
-}
+  const formatter = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 });
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await axios.get(baseUrl);
+        const { meta } = result.data;
+        setMta(meta);
+        setTotalProduct(meta.totalProduct);
+        setTotalCat(meta.totalCategory);
+
+        const { data } = result.data;
+        setError(null);
+        setProduct(data);
+      } catch (err) {
+        setTotalProduct(0);
+        setError(err.response ? err.response.data.error : err.message);
+      }
+    })();
+    let paramsUrl = baseUrl.toLowerCase().split("?")[1];
+    setSearchParams(paramsUrl);
+  }, [baseUrl, setTotalCat, setSearchParams, setTotalProduct]);
+
+  return (
+    <React.Fragment>
+      <div className="row">
+        {error ? (
+          <h1 className="mt-5">{error}</h1>
+        ) : (
+          product.map((items, i) => (
+            <div className="col-md-4">
+              <div className="card col card-product" key={i}>
+                <div className="card-product-img">
+                  <img src={items.image} alt="" />
+                </div>
+                <div className="card-body">
+                  <div className="card-product-name">{items.name}</div>
+                  <div className="card-product-price">IDR{formatter.format(items.price).split("Rp")[1]}</div>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <Pagination setPageUrl={setPageUrl} meta={mta} searchParams={searchParams} />
+    </React.Fragment>
+  );
+};
 
 export default CardProducts;
--- src/page/Product/index.jsx
+++ src/page/Product/index.jsx
@@ -1,162 +1,235 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Navbar from "../../component/Navbar/index";
 import Footer from "../../component/Footer/index";
 import CardProducts from "./CardProducts";
 
 import "./Product.css";
-import axios from "axios";
-class Product extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: [],
-      error: null,
-    };
-  }
-  async componentDidMount() {
-    try {
-      const result = await axios.get(`${process.env.REACT_APP_HOST_API}/product`);
-      this.setState({
-        product: result.data.data,
-      });
-    } catch (error) {
-      this.setState({
-        error: error.response ? error.response.data.err : error.message,
-      });
-    }
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <header className="pd-banner-wrapper">
-          <section className="pd-banner-img-background">
-            <div className="pd-banner-content-header">
-              <p>Shop {">"} Shop Right Sidebar</p>
+
+const Product = () => {
+  const [sort, setSort] = useState("");
+  const [order, setOrder] = useState("");
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [category, setCategory] = useState("");
+  const [brand, setBrand] = useState("");
+  const [totalCategory, setTotalCat] = useState([]);
+  const [totalProduct, setTotalProduct] = useState(0);
+  const [filterPrice, setFilterPrice] = useState(false);
+
+  const formatter = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", minimumFractionDigits: 0 });
+  return (
+    <React.Fragment>
+      <Navbar />
+      <header className="pd-banner-wrapper">
+        <section className="pd-banner-img-background">
+          <div className="pd-banner-content-header">
+            <p>Shop {">"} Shop Right Sidebar</p>
+          </div>
+          <div className="pd-banner-content-body">
+            <h1>Let's Shopping</h1>
+            <p>Find and buy the one you like</p>
+          </div>
+        </section>
+      </header>
+      <main className="container-main-content">
+        <aside className="pd-main-content-left">
+          <div className="pd-category-left-content">
+            <div className="pd-category-title">Categories</div>
+            {totalCategory.length &&
+              totalCategory.map((val, i) => (
+                <div className="pd-main-content-category-value" key={i}>
+                  <div
+                    className={category === val.category ? "pd-main-content-category-value-right-active" : "pd-main-content-category-value-right"}
+                    onClick={() => {
+                      setCategory(val.category);
+                    }}
+                  >
+                    {val.category}
+                  </div>
+                  <div className="pd-main-content-category-value-left">{val.total}</div>
+                </div>
+              ))}
+          </div>
+          <div className="pd-price-left-content">
+            <div className="pd-category-title">Price</div>
+            <label htmlFor="price-range">
+              Min.price <br /> IDR{formatter.format(minPrice).split("Rp")[1]}
+            </label>
+            <br />
+            <input
+              type="range"
+              name="price-range"
+              id="price-range"
+              max="20000000"
+              min="0"
+              step="1000"
+              defaultValue="0"
+              onChange={(e) => {
+                setMinPrice(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="price-range">
+              Max.price <br /> IDR{formatter.format(maxPrice).split("Rp")[1]}
+            </label>
+            <br />
+            <input
+              type="range"
+              name="price-range"
+              id="price-range"
+              max="20000000"
+              min="0"
+              step="1000"
+              defaultValue="0"
+              onChange={(e) => {
+                setMaxPrice(e.target.value);
+              }}
+            />
+          </div>
+          <div className="pd-brand-left-content">
+            <div className="pd-category-title">Brands</div>
+            <div className="input-brands-left-content">
+              <input
+                type="checkbox"
+                name="ikea"
+                id="ikea"
+                className="product-brand-input"
+                checked={brand === "ikea"}
+                onClick={() => {
+                  setBrand("ikea");
+                }}
+              />
+              <label htmlFor="ikea" className="product-brand-label">
+                IKEA
+              </label>
             </div>
-            <div className="pd-banner-content-body">
-              <h1>Let's Shopping</h1>
-              <p>Find and buy the one you like</p>
+            <div className="input-brands-left-content">
+              <input
+                type="checkbox"
+                name="mr-royal"
+                id="mr-royal"
+                className="product-brand-input"
+                checked={brand === "mr. royal"}
+                onClick={() => {
+                  setBrand("mr. royal");
+                }}
+              />
+              <label htmlFor="mr-royal" className="product-brand-label">
+                Mr. Royal
+              </label>
             </div>
-          </section>
-        </header>
-        <main className="container-main-content">
-          <aside className="pd-main-content-left">
-            <div className="pd-category-left-content">
-              <div className="pd-category-title">Categories</div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Table</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Chair</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Lamp</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Sofa</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Dining Table</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Refrigerator</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Bed</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Bathtub</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Mirror</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
-              <div className="pd-main-content-category-value">
-                <div className="pd-main-content-category-value-right">Cupboard</div>
-                <div className="pd-main-content-category-value-left">5</div>
-              </div>
+            <div className="input-brands-left-content">
+              <input
+                type="checkbox"
+                name="sweet-house"
+                id="sweet-house"
+                className="product-brand-input"
+                checked={brand === "sweet house"}
+                onClick={() => {
+                  setBrand("sweet house");
+                }}
+              />
+              <label htmlFor="sweet-house" className="product-brand-label">
+                Sweet House
+              </label>
             </div>
-            <div className="pd-price-left-content">
-              <div className="pd-category-title">Price</div>
-              <p>Price $39 - $159</p>
-              <button className="pd-button-filter">FILTER</button>
+            <div className="input-brands-left-content">
+              <input
+                type="checkbox"
+                name="north-oxford"
+                id="north-oxford"
+                className="product-brand-input"
+                checked={brand === "north oxford"}
+                onClick={() => {
+                  setBrand("north oxford");
+                }}
+              />
+              <label htmlFor="north-oxford" className="product-brand-label">
+                North Oxford
+              </label>
             </div>
-            <div className="pd-brand-left-content">
-              <div className="pd-category-title">Brands</div>
-              <div className="input-brands-left-content">
-                <input type="checkbox" name="ikea" id="ikea" className="product-brand-input" />
-                <label htmlFor="ikea" className="product-brand-label">
-                  IKEA
-                </label>
-              </div>
-              <div className="input-brands-left-content">
-                <input type="checkbox" name="mr-royal" id="mr-royal" className="product-brand-input" />
-                <label htmlFor="mr-royal" className="product-brand-label">
-                  Mr. Royal
-                </label>
-              </div>
-              <div className="input-brands-left-content">
-                <input type="checkbox" name="sweet-house" id="sweet-house" className="product-brand-input" />
-                <label htmlFor="sweet-house" className="product-brand-label">
-                  Sweet House
-                </label>
-              </div>
-              <div className="input-brands-left-content">
-                <input type="checkbox" name="north-oxford" id="north-oxford" className="product-brand-input" />
-                <label htmlFor="north-oxford" className="product-brand-label">
-                  North Oxford
-                </label>
-              </div>
-              <div className="input-brands-left-content">
-                <input type="checkbox" name="mr-poppin" id="mr-poppin" className="product-brand-input" />
-                <label htmlFor="mr-poppin" className="product-brand-label">
-                  Mr. Poppin 1929
-                </label>
-              </div>
+            <div className="input-brands-left-content">
+              <input
+                type="checkbox"
+                name="mr-poppin"
+                id="mr-poppin"
+                className="product-brand-input"
+                checked={brand === "mr. poppin 1929"}
+                onClick={() => {
+                  setBrand("mr. poppin 1929");
+                }}
+              />
+              <label htmlFor="mr-poppin" className="product-brand-label">
+                Mr. Poppin 1929
+              </label>
             </div>
-          </aside>
-          <aside className="pd-main-content-right">
-            <header className="pd-main-header-right">
-              <div>Showing 1-16 of 39 Results</div>
-              <nav className="sort-menu">
-                <ul>
-                  <li>
-                    <p>Sort by</p>
-                    <ul className="sub-sort-menu">
-                      <li>
-                        <p>Price</p>
-                      </li>
-                      <li>
-                        <p>New Items</p>
-                      </li>
-                      <li>
-                        <p>Cheapest</p>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </nav>
-            </header>
-            {this.state.error ? (
-              <p>{this.state.error}</p>
-            ) : (
-              <div className="card-wrapper row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">{this.state.product.length && this.state.product.map((item) => <CardProducts id={item.id} name={item.name} price={item.price} />)}</div>
-            )}
-          </aside>
-        </main>
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
+            <button
+              className="pd-button-filter mt-3"
+              onClick={() => {
+                setFilterPrice(true);
+              }}
+            >
+              FILTER
+            </button>
+            <button
+              className="pd-button-reset mt-3"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              RESET
+            </button>
+          </div>
+        </aside>
+        <aside className="pd-main-content-right">
+          <header className="pd-main-header-right">
+            <div>
+              Showing {totalProduct ? 1 : 0}-{totalProduct < 12 ? totalProduct : 12} of {totalProduct ? totalProduct : 0} Results
+            </div>
+            <nav className="sort-menu">
+              <ul>
+                <li>
+                  <p>Sort by</p>
+                  <ul className="sub-sort-menu">
+                    <li
+                      className="sort-value"
+                      onClick={() => {
+                        setSort("name");
+                        setOrder("asc");
+                      }}
+                    >
+                      <p>Name</p>
+                    </li>
+                    <li
+                      className="sort-value"
+                      onClick={() => {
+                        setOrder("desc");
+                        setSort("created_at");
+                      }}
+                    >
+                      <p>New Items</p>
+                    </li>
+                    <li
+                      className="sort-value"
+                      onClick={() => {
+                        setOrder("asc");
+                        setSort("price");
+                      }}
+                    >
+                      <p>Cheapest</p>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <CardProducts sort={sort} order={order} brand={brand} setTotalCat={setTotalCat} category={category} minPrice={minPrice} maxPrice={maxPrice} filterPrice={filterPrice} setTotalProduct={setTotalProduct} />
+        </aside>
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default Product;
