9a69b5f078085f31e19018086528f48695f05a3b
kondelow <achmadrasidi@gmail.com>
2022-06-24 11:59:46 +0700

    pull from staging

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Provider as ReduxProvider } from "react-redux"
-import { store, persistor } from "./redux/store"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 // import './App.css';
-import Login from './page/Login';
-import Footer from "./component/Footer"
-import Home from './page/Home';
-import Profile from './page/Profile';
-
-
-
+import Login from "./page/Login";
+import Footer from "./component/Footer";
+import Home from "./page/Home";
+import Profile from "./page/Profile";
 
 function App() {
   return (
@@ -17,10 +14,10 @@ function App() {
       <PersistGate loading={null} persistor={persistor}>
         <Router>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/auth' element={<Login />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/footer' element={<Footer />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/footer" element={<Footer />} />
             {/* <Route path='/navbar' element={<Navbar />} /> */}
           </Routes>
         </Router>
