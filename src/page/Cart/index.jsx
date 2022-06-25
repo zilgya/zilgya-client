e8316342405f9341488ef1ed9c21ac3bb83a2af3
Yoga Arta G <grahanantyo@gmail.com>
2022-06-26 03:09:39 +0700

    add implementation product detail and add to cart

--- src/page/Cart/index.jsx
+++ src/page/Cart/index.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 import Footer from '../../component/Footer';
 import Navbar from '../../component/Navbar';
 
-import CartProductOne from '../../assets/img/cart-product-1.png'
+// import CartProductOne from '../../assets/img/cart-product-1.png'
 // import CartProductTwo from '../../assets/img/cart-product-2.png'
 
 import './Cart.css'
+import { connect } from 'react-redux';
+import { currencyFormatter } from '../../helper/currencyFormatter';
+import { counterDownByIdAction, counterUpByIdAction, deleteCartAction, deleteFromCartAction } from '../../redux/actionCreator/cart';
 class Cart extends Component {
   constructor() {
     super();
@@ -27,7 +30,7 @@ class Cart extends Component {
               <div className="co-header-info">Buy everything in your cart now!</div>
             </div>
           </div>
-          {this.state.cartItem.length === 0 ?
+          {this.props.cartItem.length === 0 ?
             <main className='cart-container-empty'>
               <CartIcon className='cart-icon' />
               <div className="cart-empty-title">Your Cart is Empty </div>
@@ -45,20 +48,34 @@ class Cart extends Component {
                   <div className='cart-main-content-left-title-total'>TOTAL</div>
                 </div>
                 <div className='cart-main-content-left-body'>
-                  <div className="cart-main-product">
-                    <div className='cart-main-content-product-delete-img-name'>
-                      <XLg className='cart-main-content-delete' />
-                      <img src={CartProductOne} alt="product-img" className='cart-main-content-img' />
-                      <div className='cart-main-content-product-name'>Fabric Mid Century Chair</div>
+                  {this.props.cartItem.map(cart => (
+                    <div className="cart-main-product">
+                      <div className='cart-main-content-product-delete-img-name'>
+                        <XLg className='cart-main-content-delete'
+                          onClick={() => {
+                            this.props.dispatch(deleteFromCartAction(cart.id))
+                          }}
+                        />
+                        <img src={cart.pict.url} alt="product-img" className='cart-main-content-img' />
+                        <div className='cart-main-content-product-name'>{cart.name}</div>
+                      </div>
+                      <div className='cart-main-content-product-price'>{currencyFormatter.format(cart.price)}</div>
+                      <div className='cart-main-content-product-wrapper-quantity'>
+                        <div className='cart-main-content-plus-minus'
+                        onClick={()=>{
+                          this.props.dispatch(counterDownByIdAction(cart.id))
+                        }}
+                        >-</div>
+                        <div className='cart-main-content-product-quantity'>{cart.quantity}</div>
+                        <div className='cart-main-content-plus-minus'
+                        onClick={()=>{
+                          this.props.dispatch(counterUpByIdAction(cart.id))
+                        }}
+                        >+</div>
+                      </div>
+                      <div className='cart-main-content-product-total'>{currencyFormatter.format(cart.price * cart.quantity)}</div>
                     </div>
-                    <div className='cart-main-content-product-price'>$10.50</div>
-                    <div className='cart-main-content-product-wrapper-quantity'>
-                      <div className='cart-main-content-plus-minus'>-</div>
-                      <div className='cart-main-content-product-quantity'>02</div>
-                      <div className='cart-main-content-plus-minus'>+</div>
-                    </div>
-                    <div className='cart-main-content-product-total'>$21.00</div>
-                  </div>
+                  ))}
                 </div>
                 <div className='cart-main-content-left-footer'>
                   <div className='cart-main-content-promo'>
@@ -66,8 +83,12 @@ class Cart extends Component {
                     <div className='cart-apply-promo'>Apply Coupon</div>
                   </div>
                   <div className='cart-main-content-clear-update'>
-                    <div className='cart-clear'>Clear Cart</div>
-                    <div className='cart-update'>Update Cart</div>
+                    <div className='cart-clear'
+                      onClick={() => {
+                        this.props.dispatch(deleteCartAction())
+                      }}
+                    >Clear Cart</div>
+                    <div className='cart-update'><Link to="/product">Update Cart</Link></div>
                   </div>
                 </div>
               </div>
@@ -111,4 +132,11 @@ class Cart extends Component {
   }
 }
 
-export default Cart;
+const mapStateToProps = (reduxState) => {
+  const { cart: { cartItem } } = reduxState
+  return {
+    cartItem
+  }
+}
+
+export default connect(mapStateToProps)(Cart);
--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
+import { cartReducer } from "./cart";
 import { searchReducer } from "./search";
 import UserReducer from "./user";
 
@@ -7,6 +8,7 @@ const reducers = combineReducers({
   auth: authReducer,
   user: UserReducer,
   search: searchReducer,
+  cart: cartReducer
 });
 
 export default reducers;
