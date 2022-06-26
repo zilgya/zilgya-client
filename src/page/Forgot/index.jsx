19a3361dbd3136c98d697ea807885c654d97f720
kondelow <achmadrasidi@gmail.com>
2022-06-26 20:34:17 +0700

    add route forgot

--- src/page/Forgot/index.jsx
+++ src/page/Forgot/index.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import Footer from '../../component/Footer'
-import Navbar from '../../component/Navbar'
-import './Forgot.css'
+import React, { Component } from "react";
+import Footer from "../../component/Footer";
+import Navbar from "../../component/Navbar";
+import "./Forgot.css";
 
 export default class Forgot extends Component {
   render() {
@@ -11,23 +11,17 @@ export default class Forgot extends Component {
         <main className="login-global-container">
           <div className="login-header">
             <div className="login-header-title">My Account</div>
-            <div className="login-header-info">
-              Register and log in with your account to be able to shop at will
-            </div>
+            <div className="login-header-info">Register and log in with your account to be able to shop at will</div>
           </div>
           <div className="forgot-main-container">
-            <div className="login-register-title">
-              Forgot your Password?
-            </div>
-            <div className="forgot-main-info">
-              Don't worry! Just fill in your email and we'll send you a link
-            </div>
-            <input type="text" className="forgot-input" placeholder='Your email address'/>
-            <input type="submit" value="Reset Password" className="forgot-button"/>
+            <div className="login-register-title">Forgot your Password?</div>
+            <div className="forgot-main-info">Don't worry! Just fill in your email and we'll send you a link</div>
+            <input type="text" className="forgot-input" placeholder="Your email address" />
+            <input type="submit" value="Reset Password" className="forgot-button" />
           </div>
         </main>
         <Footer />
       </>
-    )
+    );
   }
 }
